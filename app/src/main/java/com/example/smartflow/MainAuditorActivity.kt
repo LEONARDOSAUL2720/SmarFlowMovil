75e3fee0a8a44095c34a655ca1085ca2b0ce673d
LEONARDOSAUL2720 <avilasanchezleonardosaul2@gmail.com>
2025-07-11 18:49:07 -0600

    direcionamiento para empleado y auditor listo

---
+++ app/src/main/java/com/example/smartflow/MainAuditorActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smartflow
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainAuditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_auditor)
+        // ...existing code...
+    }
+}
